56a394df7da8834932e936f682df9326cb3afff0
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-11 16:51:11 +0000

    Rename print-html-step-5 to http-functions.js

---
+++ http-functions.js
@@ -0,0 +1,20 @@
+function getHTML (options, callback) {
+  var https = require('https');
+  https.get(options, printHTML);
+};
+
+function printHTML (res) {
+  let outString = "";
+
+  res.on("data", function(chunk){
+    outString += chunk.toString();
+  })
+
+  res.on("end", function(){
+    console.log(outString);
+  })
+};
+
+module.exports = {
+  "getHTML": getHTML
+}
